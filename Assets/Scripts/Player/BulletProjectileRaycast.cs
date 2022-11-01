7beae212e4a33b516c9e9a9374da3a346d4788bc
cqtuan <tuancq2011@gmail.com>
2022-11-01 19:11:40 +0700

    import packages

---
+++ Assets/Scripts/Player/BulletProjectileRaycast.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletProjectileRaycast : MonoBehaviour
+{
+    public GameObject impactParticle; // Effect spawned when projectile hits a collider
+    public GameObject projectileParticle; // Effect attached to the gameobject as child
+    public GameObject muzzleParticle; // Effect instantly spawned when gameobject is spawned
+    [SerializeField] private float shootSpeed = 200f;
+
+    private Vector3 targetPosition;
+
+    //private void Start()
+    //{
+    //    projectileParticle = Instantiate(projectileParticle) as GameObject;
+    //    projectileParticle.transform.SetParent(transform, false);
+
+    //    if (muzzleParticle)
+    //    {
+    //        muzzleParticle = Instantiate(muzzleParticle, transform.position, transform.rotation) as GameObject;
+    //        Destroy(muzzleParticle, 1.5f); // 2nd parameter is lifetime of effect in seconds
+    //    }
+    //}
+
+    public void Setup(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+
+    private void Update()
+    {
+        Vector3 moveDirection = (targetPosition - transform.position).normalized;
+        transform.position += moveDirection * shootSpeed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
